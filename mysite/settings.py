67991e66564a8ee4839d4049681723667edc9515
jersonlopez <jersonlopez.1204@gmail.com>
2018-03-24 13:06:54 -0500

    sin bd en settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -89,14 +89,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 #    }
 #}
 
-# keep this
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # add this
 import dj_database_url
 db_from_env = dj_database_url.config()
